8e7bbbc5839455a2afa63b221c6f0fece06c563f
yuukiwaw <zhangtuo1999@gmail.com>
2021-10-28 14:22:21 +0800

    更新遍历方法

--- datahub.py
+++ datahub.py
@@ -13,25 +13,32 @@ from DataHubLibV1 import *
 
 
 def main():
-    hostname = "localhost"
-    port = "4502"
-    domain = "DataPid"
-    
-    #! 示例话DataHubClass对象并且继承DataHubEventConsumer类
-    DataHub = DataHubClass(DataHubEventConsumer)
-    #! 创建连接
-    DataHub.connect(hostname, port, domain)
-    while 1:
-        time.sleep(1)
-        #? 终端清屏
-        print("\033c", end="")
-        print("#########################" + time.asctime() + "#########################" + "\n")
-        #? 获取Data（返回list格式）
-        DataHub_List = DataHub.getDataHub_Data()
-        for item in DataHub_List:
-            DataHub_Data_Dict = json.loads(item);
-            print (DataHub_Data_Dict)
-
+    try:
+        hostname = "localhost"
+        port = "4502"
+        domain = "DataPid"
+        
+        #! 示例话DataHubClass对象并且继承DataHubEventConsumer类
+        DataHub = DataHubClass(DataHubEventConsumer)
+        #! 创建连接
+        DataHub.connect(hostname, port, domain)
+        while 1:
+            time.sleep(1)
+            #? 终端清屏
+            print("\033c", end="")
+            print("#########################" + time.asctime() + "#########################" + "\n")
+            #? 获取Data（返回list格式）
+            DataHub_List = DataHub.getDataHub_Data()
+            
+            for item in range(len(DataHub_List)):
+                print(DataHub_List[item])
+            # for item in DataHub_List:
+            #     # DataHub_Data_Dict = json.loads(item);
+            #     # print (DataHub_Data_Dict)
+            #     print(item)
+            
+    except Exception as ex:
+        print(ex)
 
 if __name__ == '__main__':
     main()
