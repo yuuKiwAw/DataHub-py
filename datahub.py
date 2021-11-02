ab60e73011d87caf07ca3cf883b6309da461d313
yuukiwaw <zhangtuo1999@gmail.com>
2021-11-02 16:32:26 +0800

    新增错误处理抛出并重新启动以及日志输出系统

--- datahub.py
+++ datahub.py
@@ -2,7 +2,12 @@
 
 import clr
 import time
-import json
+import logging
+import os.path
+import sys
+import signal
+from pydantic import BaseModel
+from retrying import retry
 
 clr.FindAssembly("Cogent.dll")
 clr.AddReference("Cogent")
@@ -12,18 +17,46 @@ clr.AddReference("DataHubLibV1")
 from DataHubLibV1 import *
 
 
-def main():
+class DataHubSet(BaseModel):
+    hostname: str
+    port: str
+    domain: str
+
+
+#! logging日志模块
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+log_path = os.path.dirname(os.getcwd()) + '/DataHub/logs/'
+file_name = 'datahub_errors_' + time.strftime('%Y-%m-%d-%H-%M', time.localtime(time.time())) + '.log'
+
+fh = logging.FileHandler(log_path + file_name, mode='w', encoding='utf-8')
+fh.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+#! logging日志模块
+
+
+@retry()
+def GetDataHubInfo(DataHubSet):
+    """ 获取DataHub数据（带异常抛出重试）
+    Args:
+        DataHubSet ([type]): [description]
+    """
+    dhs = DataHubSet
+    hostname = dhs.hostname
+    port = dhs.port
+    domain = dhs.domain
     try:
-        hostname = "localhost"
-        port = "4502"
-        domain = "DataPid"
-        
         #! 示例话DataHubClass对象并且继承DataHubEventConsumer类
         DataHub = DataHubClass(DataHubEventConsumer)
         #! 创建连接
         DataHub.connect(hostname, port, domain)
+        logger.info('Start Connection')
+        
         while 1:
-            time.sleep(1)
+            time.sleep(0.1)
             #? 终端清屏
             print("\033c", end="")
             print("#########################" + time.asctime() + "#########################" + "\n")
@@ -32,13 +65,23 @@ def main():
             
             for item in range(len(DataHub_List)):
                 print(DataHub_List[item])
-            # for item in DataHub_List:
-            #     # DataHub_Data_Dict = json.loads(item);
-            #     # print (DataHub_Data_Dict)
-            #     print(item)
-            
+    #! 异常处理抛出
     except Exception as ex:
+        logger.error(ex)
         print(ex)
+        raise Exception
+    except KeyboardInterrupt:
+        logger.warning('Stop manually!')
+        pass
+
+
+def main():
+    dhs_s1 = DataHubSet
+    dhs_s1.hostname = "localhost"
+    dhs_s1.port = "4502"
+    dhs_s1.domain = "DataPid"
+    
+    GetDataHubInfo(dhs_s1)
 
 if __name__ == '__main__':
     main()
